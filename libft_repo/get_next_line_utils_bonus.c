93c143b88510ecf5aa9aafc1c7018ba2ddbd50d3
Nadege ALAVOINE <anadege@e1r6p4.42.fr>
2021-05-24 17:41:41 +0200

    extra functions in gnl removed

--- libft_repo/get_next_line_utils_bonus.c
+++ libft_repo/get_next_line_utils_bonus.c
@@ -6,59 +6,11 @@
 /*   By: elanna <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 17:01:55 by elanna            #+#    #+#             */
-/*   Updated: 2021/05/24 16:42:42 by anadege          ###   ########.fr       */
+/*   Updated: 2021/05/24 17:39:22 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
-
-char	*ft_strdup(const char *s)
-{
-	int		size;
-	char	*dest;
-	int		i;
-
-	i = 0;
-	size = 0;
-	while (s && s[size])
-		size++;
-	dest = malloc(sizeof(*dest) * (size + 1));
-	if (!dest)
-		return (0);
-	while (s[i] != 0)
-	{
-		dest[i] = s[i];
-		i++;
-	}
-	dest[i] = 0;
-	return (dest);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	i;
-	size_t	size;
-	char	*str;
-
-	i = -1;
-	size = 0;
-	if (!s1 && !s2)
-		return (NULL);
-	while (s1 && s1[size])
-		size++;
-	while (s2 && s2[++i])
-		size++;
-	str = malloc(sizeof(*str) * (size + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s1 && *s1)
-		str[i++] = *s1++;
-	while (s2 && *s2)
-		str[i++] = *s2++;
-	str[size] = 0;
-	return (str);
-}
+#include "libft_bonus.h"
 
 t_content	*ft_contnew(char *buffer, int fd)
 {
